728000024be19e7a5c8fdad5b1458ce4daf54315
Marina <95050756+Marindala@users.noreply.github.com>
2023-08-16 12:32:11 -0300

    process to add images

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import { useState } from 'react'
 import { createClient } from '@supabase/supabase-js'
 
-export async function getStaticProps() {
+/* export async function getStaticProps() {
   const supabaseAdmin = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL || '',
     process.env.SUPABASE_SERVICE_ROLE_KEY || ''
@@ -14,7 +14,7 @@ export async function getStaticProps() {
       images: data,
     },
   }
-}
+} */
 
 function cn(...classes: string[]) {
   return classes.filter(Boolean).join(' ')
@@ -32,7 +32,7 @@ export default function Gallery({ images }: { images: Image[] }) {
   return (
     <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
       <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-       <BlurImage/>
+     <h1>Ver imagenes</h1>
       </div>
     </div>
   )
