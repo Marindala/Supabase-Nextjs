addda6dffda03d674dbde4adb7c69d528bba247c
Marina <95050756+Marindala@users.noreply.github.com>
2023-08-16 19:18:56 -0300

    yes! in back!

--- next-config.js
+++ next-config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-    reactStrictMode: true,
-    images: {
-      domains: ['pbs.twimg.com', 'encrypted-tbn0.gstatic.com', 'instagram.feze11-1.fna.fbcdn.net'],
-    },
-  }
+  reactStrictMode: true,
+  images: {
+    domains: ['scontent.feze11-1.fna.fbcdn.net'],
+  },
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import { useState } from 'react'
 import { createClient } from '@supabase/supabase-js'
 
- export async function getStaticProps() {
+export async function getStaticProps() {
   const supabaseAdmin = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL || '',
     process.env.SUPABASE_SERVICE_ROLE_KEY || ''
@@ -14,7 +14,7 @@ import { createClient } from '@supabase/supabase-js'
       images: data,
     },
   }
-} 
+}
 
 function cn(...classes: string[]) {
   return classes.filter(Boolean).join(' ')
@@ -26,44 +26,76 @@ type Image = {
   imageSrc: string
   name: string
   username: string
+  image: string
 }
 
 export default function Gallery({ images }: { images: Image[] }) {
-  //const [isLoading, setLoading] = useState(true)
   return (
-    <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-      <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-      <BlurImage/>
-     
-
-
+    <div>
+       <h1>Galeria de Arte </h1>
+    
+   
+    <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 bg-pink-300">
+      
+      <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        
+        {images.map((image) => (
+          <BlurImage key={image.id} image={image} />
+        ))}
       </div>
+    </div>
     </div>
   )
 }
 
-function BlurImage() {
+function BlurImage({ image }: { image: Image }) {
   const [isLoading, setLoading] = useState(true)
 
   return (
-    <a href="#" className="group">
-      <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+    
+    <a href={image.href} className="group">
+      <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+        <img
+          alt=""
+          src={image.imageSrc}
+          style={{
+            objectFit: 'cover',
+            width: '100%',
+            height: '100%',
+          }}
+          className={cn(
+            'duration-700 ease-in-out group-hover:opacity-75',
+            isLoading
+              ? 'scale-110 blur-2xl grayscale'
+              : 'scale-100 blur-0 grayscale-0'
+          )}
+          onLoad={() => setLoading(false)}
+        />
+      </div>
+      <h3 className="mt-4 text-sm text-gray-700">{image.name}</h3>
+      <p className="mt-1 text-lg font-medium text-gray-900">{image.username}</p>
+    </a>
+
+    /*  <a href="#" className="group">
+      
+      <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7 z-40">
         <img
           alt=""
           src="https://scontent.feze11-1.fna.fbcdn.net/v/t39.30808-6/339136257_917559112721773_1741361305998165288_n.jpg?stp=dst-jpg_p600x600&_nc_cat=104&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeHI4Rirrp6I3Uz_q3NMGtY3IKSXKwIJ7xogpJcrAgnvGrRXz7l-blGD0pKxH4M0Y77wQ0BSgQ6-zVGkLiQlaKR-&_nc_ohc=8yWyDbHswBQAX9CHWVW&_nc_ht=scontent.feze11-1.fna&oh=00_AfCv-uBHVeeWqgWFaRXs0q-CT8ijCmnU4e_qPJe-Aul-rQ&oe=64E11CAB"
-          style={{ objectFit: 'cover',
-          width: '100%',
-          height: '100%',
-          transition: 'transform 0.7s ease-in-out',
-          transform: isLoading ? 'scale(1.1) blur(2px) grayscale(1)' : 'scale(1) blur(0) grayscale(0)',
-        }}
-          
-        
-        onLoad={() => setLoading(false)}
-        /> 
+          style={{
+            objectFit: 'cover',
+            width: '100%',
+            height: '100%',
+            transition: 'transform 0.7s ease-in-out',
+            transform: isLoading
+              ? 'scale(1.1) blur(2px) grayscale(1)'
+              : 'scale(1) blur(0) grayscale(0)',
+          }}
+          onLoad={() => setLoading(false)}
+        />
       </div>
       <h3 className="mt-4 text-sm text-gray-700">Nombre</h3>
       <p className="mt-1 text-lg font-medium text-gray-900">@tanto</p>
-    </a>
+    </a> */
   )
 }
