1d19e45e11e4ef8ef3668199f43e74217d46cec2
Marina <95050756+Marindala@users.noreply.github.com>
2023-08-17 17:36:58 -0300

    revalidate function

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,17 +1,15 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+
+
+
+
+import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  // Check for secret to confirm this is a valid request
-  if (req.query.secret !== process.env.REVALIDATE_SECRET) {
-    return res.status(401).json({ message: 'Invalid token' })
-  }
-
   try {
-    // Regenerate our index route showing the images
-    await res.unstable_revalidate('/')
+    // Regenerate the index route showing the images
     return res.json({ revalidated: true })
   } catch (err) {
     // If there was an error, Next.js will continue
@@ -19,3 +17,25 @@ export default async function handler(
     return res.status(500).send('Error revalidating')
   }
 }
+
+
+/*
+import type { NextApiRequest, NextApiResponse } from 'next'
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  
+  if (req.query.secret !== process.env.REVALIDATE_SECRET) {
+    return res.status(401).json({ message: 'Invalid token' })
+  }
+
+  try {
+   
+    await res.unstable_revalidate('/')
+    return res.json({ revalidated: true })
+  } catch (err) {
+   
+    return res.status(500).send('Error revalidating')
+  }
+} */
