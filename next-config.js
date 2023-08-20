aead984a57950d73524ebe2885c8d2dadc4cd85a
Marina <95050756+Marindala@users.noreply.github.com>
2023-08-20 19:04:43 -0300

    resolved next-config

--- next-config.js
+++ next-config.js
@@ -5,3 +5,5 @@ module.exports = {
     domains: ['scontent.feze11-1.fna.fbcdn.net', 'facebook.com', 'scontent.fepa10-1.fna.fbcdn.net'],
   },
 };
+
+/* El error "URL signature expired" suele ocurrir cuando se utiliza un enlace (URL) firmado con un token de acceso temporal, y ese token ha caducado. Supongo que estás utilizando Supabase para generar enlaces firmados que permiten acceder a imágenes almacenadas en algún lugar, y estos enlaces tienen un tiempo limitado de validez para aumentar la seguridad. */
