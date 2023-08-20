9b80e0a24a4df3c658f87e1a00988af5def9942c
Marina <95050756+Marindala@users.noreply.github.com>
2023-08-20 19:44:20 -0300

    head title logo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/global.css'
+import Head from "next/head";
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <title>Art Gallery</title>
+        <meta name="description" content="Art Gallery" />
+        <link rel="icon" href="/logopng.png" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
